9e88f9cb1024982cfe04268da793c359091e952f
kaioalex <caioalex2017@gmail.com>
2023-11-21 18:32:20 -0300

    model and app modularized

--- model.py
+++ model.py
@@ -3,11 +3,12 @@ from sklearn.metrics.pairwise import linear_kernel
 
 import csv
 import pandas as pd
+import pickle
+from datetime import datetime
 
 from itertools import groupby
 from operator import itemgetter
 
-# Assuming you have playlists_df and songs_df DataFrames
 reader = csv.DictReader(open("./datasets/2023_spotify_ds1.csv"), delimiter=",")
 songs_list = []
 
@@ -24,7 +25,6 @@ for key, value in groupby(songs_list, key = itemgetter('pid')):
 	for k in value:
 		l.append(k['track_name'])
 
-	#data.append(l)
 	data[key] = l
 
 playlists_df = pd.DataFrame(list(data.items()), columns=['pids', 'songs'])
@@ -36,26 +36,24 @@ playlists_df['combined_songs'] = playlists_df['songs'].apply(lambda x: ' '.join(
 tfidf_vectorizer = TfidfVectorizer()
 playlist_tfidf_matrix = tfidf_vectorizer.fit_transform(playlists_df['combined_songs'])
 
-# Vectorize user input songs
-user_input_songs = ["Ride Wit Me", "I Got the Keys"]
-user_input_vector = tfidf_vectorizer.transform([" ".join(user_input_songs)])
+version = 0
+try:
+	file = open("./trained_model.pickle", "rb")
+	last_model = pickle.load(file)
+	version = last_model['version'] + 1
+except:
+	pass
 
-# Calculate cosine similarity
-cosine_similarities = linear_kernel(user_input_vector, playlist_tfidf_matrix).flatten()
+persist = {
+	"playlists_df": playlists_df,
+	"playlist_tfidf_matrix": playlist_tfidf_matrix,
+	"tfidf_vectorizer": tfidf_vectorizer,
+	"version": version,
+	"model_date": datetime.now().isoformat()
+}
 
-# Recommend playlists based on similarity scores
-#playlist_recommendations = playlists_df.loc[cosine_similarities.argsort()[::-1]]
+file = open("./trained_model.pickle", "wb")
+pickle.dump(persist, file)
+file.close()
 
-# Rank playlists based on similarity scores
-playlist_ranking = cosine_similarities.argsort()[::-1]
-
-import random
-
-N = random.randint(2, 10)  # You can replace this with any desired value
-
-# Recommend the top-N playlists
-top_playlists = playlists_df.iloc[playlist_ranking[:N]]
-
-# Display the recommended playlists
-print(f"Top-{N} Recommended Playlists:")
-print(top_playlists[['pids']].to_string(index=False))
+print("Trained model saved: 'trained_model.pickle'")
